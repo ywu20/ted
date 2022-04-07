30a6e67d53dcb8e7dae739a12ab3566aa9637692
Yuqing Wu <ywu20@stuy.edu>
2022-04-07 09:19:20 -0400

    add print statement to show states of server in terminal

--- s.py
+++ s.py
@@ -1,13 +1,6 @@
 import asyncio
-
 import json
-
-import logging
-
 import websockets
-
-logging.basicConfig()
-
 
 USERS = []
 
@@ -19,38 +12,33 @@ def value_event(message):
     message = message.split(" ")
     coor = message[1]
     coor = coor.split(",")
+    print("server received mode: " +message[0] +". mouse click at: "+ coor[0]+","+coor[1])
     return json.dumps({"mode":message[0] , "xcor": coor[0], "ycor" : coor[1]})
 
-
-async def counter(websocket):
-
-    global USERS, VALUE
-
+async def send_data(websocket):
     try:
-
         # Register user
-
         USERS.append(websocket)
-
+        print("New user entered!")
+        # Tell all the clients about the new user.
         websockets.broadcast(USERS, users_event())
 
-        # Manage state changes
+        # Tell clients about somebody clicking on canvas.
         async for message in websocket:
             websockets.broadcast(USERS, value_event(message))
 
     finally:
         # Unregister user
         USERS.remove(websocket)
+        print("Some user exited.")
         websockets.broadcast(USERS, users_event())
 
 
 async def main():
-
-    async with websockets.serve(counter, "localhost", 6789):
-
+    # server at localhost 6789
+    async with websockets.serve(send_data, "localhost", 6789):
         await asyncio.Future()  # run forever
 
 
 if __name__ == "__main__":
-
     asyncio.run(main())
