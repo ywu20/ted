e33137ffcd5065d21bfab4035b0d29f7b73fe129
AnnabelZhang1 <annabel.zhang1@gmail.com>
2022-06-06 14:45:42 -0400

    changed some things

--- __init__.py
+++ __init__.py
@@ -1,32 +1,26 @@
+import json
 from flask import *
 from flask_socketio import *
 
+USERS = []
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app)
+app.config['SECRET_KEY'] = 'some super secret key!'
+socketio = SocketIO(app, cors_allowed_origins='*')
 
-# site routes
 @app.route("/")
 def boo():
     return render_template('hello.html')
 
-# socketio events
-# @socketio.on('my event')
-# def test_message(message):
-#     emit('my response', {'data': message['data']})
+@socketio.event
+def my_ping():
+    emit('my_pong', {'text': 'Ping pong!'})
 
-@socketio.on('send_message')
+@socketio.on('send_mouse')
 def message_recieved(data):
-    print(data['text'])
+    # print(data['text'])
+    emit('draw_to_all', data, broadcast=True)
+    # emit('message_from_server', {'text':'Message recieved!'})
 
-@socketio.on('connect')
-def test_connect():
-    emit('my response', {'data': 'Connected'})
 
-@socketio.on('disconnect')
-def test_disconnect():
-    print('Client disconnected')
-
-if __name__ == '__main__':
-    # socketio.run(app)
-    app.run()
+if __name__ == "__main__":
+    socketio.run(app, port=8000, debug=True)
